87be9b88a744f4fc872a7307ff570659ea5f2187
kishanverma24 <kishanverma9151@gmail.com>
2024-07-02 15:45:00 +0530

    me

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -12,13 +12,13 @@ const Contact = () => {
           <h1>Contact Me</h1>
 
           <h2>
-            <img src="/contact/linkedin.jpg" alt="linkedin logo" />{" "}
-            Linked In: "www.linkedin.com/in/kishan-verma-00478a310"{" "}
+            <img src="/contact/linkedin.jpg" alt="linkedin logo" /> Linked In:
+            "www.linkedin.com/in/kishan-verma-00478a310 "{" "}
             <a href="www.linkedin.com/in/kishan-verma-00478a310">Connect</a>{" "}
           </h2>
           <h2>
-            <img src="/contact/github.png" alt="github logo" /> Github
-            Account: "https://github.com/kishanverma24"{" "}
+            <img src="/contact/github.png" alt="github logo" /> Github Account:
+            "https://github.com/kishanverma24"{" "}
             <a href="https://github.com/kishanverma24">View</a>{" "}
           </h2>
           <h2>
@@ -30,8 +30,8 @@ const Contact = () => {
             Email Id: "kishanverma9151@gmail.com"{" "}
           </h2>
           <h2>
-            <img src="/contact/contact.jpg" alt="contact logo" /> Contact
-            & WhatsApp no: 9151634126{" "}
+            <img src="/contact/contact.jpg" alt="contact logo" /> Contact &
+            WhatsApp no: 9151634126{" "}
           </h2>
         </div>
       </div>
